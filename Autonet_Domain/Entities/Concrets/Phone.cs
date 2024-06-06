6990a33f113729ae6e451b7a9a34bf2e9ca40f95
aminb <aminb@ASUSROG>
2024-06-06 23:22:47 +0400

    Commit

---
+++ Autonet_Domain/Entities/Concrets/Phone.cs
@@ -0,0 +1,21 @@
+﻿using Autonet_Domain.Entities.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autonet_Domain.Entities.Concrets
+{
+    public class Phone:BaseEntity
+    {
+        public string? Phone_ {  get; set; }
+
+        //Navigaton Property
+        public Seller? Seller { get; set;}
+
+        // Foreign Key
+        public int SellerId { get; set; }
+
+    }
+}
---
+++ Autonet_Domain/Entities/Concrets/Seller.cs
@@ -0,0 +1,19 @@
+﻿using Autonet_Domain.Entities.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autonet_Domain.Entities.Concrets;
+
+public class Seller:BaseEntity
+{
+    public string? Username { get; set; }
+    
+    // Navigation Property
+    public ICollection<Plate?>? Plates { get; set; }
+    public ICollection<Phone>? Phones { get; set; }
+
+
+}
